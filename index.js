7dafb874db0768a9cbf8c136f6fea02b7d7fe0da
Fabricio Campos <fabricio@vizir.com.br>
2019-05-14 20:01:43 -0300

    return mocha rules
    Mocha rules was removed because we release a new minor version. Now we are releasing a new major, because this version breaks the previous.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const rules = require('./src/rules');
 const overrides = require('./src/overrides');
+const mocharules = require('./src/mocha/rules');
 
 module.exports = {
   extends: ['airbnb-base'],
-  rules,
+  plugins: ['mocha'],
+  rules: Object.assign(rules, mocharules),
   overrides,
 };
